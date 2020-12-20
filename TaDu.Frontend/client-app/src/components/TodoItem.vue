77b737544ffb47878ae3c2c5bdc6e9b97217919c
ThanhCuD <thanhduc552018@gmail.com>
2020-12-20 22:15:11 +0700

    'new'

---
+++ TaDu.Frontend/client-app/src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <li>
+        <span>
+            {{item.text}} 
+        </span>
+        <button @click="$emit('Remove',Number(item.id))">X</button>
+      </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: {
+    item : Object
+  },
+  data () {
+    return{
+      data :"item"
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
