026b676ec8b78dba98e42e0ee91a47ddfb01e6f4
ThanhDuc <thanh.duc@websparks.sg>
2020-12-21 17:38:38 +0700

    add

--- TaDu.Frontend/client-app/src/components/Registration.vue
+++ TaDu.Frontend/client-app/src/components/Registration.vue
@@ -60,6 +60,30 @@ export default {
       //  axios
       // .get('https://api.coindesk.com/v1/bpi/currentprice.json')
       // .then(response => (console.log(response)))
+
+      //
+// fetch("http://localhost:3001/SupportingDocuments/Save", {
+//   "headers": {
+//     "accept": "application/json, text/plain, */*",
+//     "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+//     "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI4N2VkZTc2Ni0xYTBhLTRjMjEtYmFjZS1jNGYwNTVjMTUyODQiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IlNETyIsIm5iZiI6MTYwODI4NzU4OSwiZXhwIjoxNjA4MzczOTg5LCJpYXQiOjE2MDgyODc1ODl9.5o8dgGYBqecJ6PhHvt2-MmY2OD_79hkVEXZ0iVGtBLQ",
+//     "cache-control": "no-cache",
+//     "content-type": "application/json;charset=UTF-8",
+//     "pragma": "no-cache",
+//     "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
+//     "sec-ch-ua-mobile": "?0",
+//     "sec-fetch-dest": "empty",
+//     "sec-fetch-mode": "cors",
+//     "sec-fetch-site": "same-origin"
+//   },
+//   "referrer": "http://localhost:3001/search-case/edit-case/step/6",
+//   "referrerPolicy": "strict-origin-when-cross-origin",
+//   "body": "{\"caseId\":59,\"personalDocs\":[{\"householdMemberId\":46,\"uploadedFiles\":[{\"selectedDocumentTypeId\":353,\"uploadedFileId\":465}]}],\"loggedUserId\":1}",
+//   "method": "POST",
+//   "mode": "cors",
+//   "credentials": "include"
+// });
+      //
       async function postData(url = '', data = {}) {
           // Default options are marked with *
         const response = await fetch(url, {
