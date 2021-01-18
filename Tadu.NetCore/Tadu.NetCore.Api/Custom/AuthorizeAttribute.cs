577dbfb02afddc5046688db22a87db0d38667672
ThanhCuD <thanhduc552018@gmail.com>
2021-01-18 22:56:09 +0700

    add jwt

---
+++ Tadu.NetCore/Tadu.NetCore.Api/Custom/AuthorizeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace Tadu.NetCore.Api.Custom
+{
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.Items["User"];
+            if (user == null)
+            {
+                context.Result = new JsonResult(new { message = "Unauthorized" })
+                {
+                    StatusCode = StatusCodes.Status401Unauthorized
+                };
+            }
+        }
+    }
+}
---
+++ Tadu.NetCore/Tadu.NetCore.Api/Custom/JwtMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tadu.NetCore.Data.Services;
+using Tadu.NetCore.Global.Helper;
+
+namespace Tadu.NetCore.Api.Custom
+{
+    //https://jasonwatmore.com/post/2019/10/11/aspnet-core-3-jwt-authentication-tutorial-with-example-api#authorize-attribute-cs
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly AppSettings _appSettings;
+
+        public JwtMiddleware(RequestDelegate next, IOptions<AppSettings> options)
+        {
+            _next = next;
+            _appSettings = options.Value;
+        }
+
+
+        private void AttachUserToContext(HttpContext context, IUserService userService, string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+                tokenHandler.ValidateToken(token, new TokenValidationParameters()
+                {
+                    //todo
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var userId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+
+                context.Items["User"] = userService.GetUserById(userId.ToString());
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
